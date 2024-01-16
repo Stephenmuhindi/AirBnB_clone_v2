812da7ff2dbb55b870934253d482eaf7cd7c5110
Zablon Samba <zablonsamba@gmail.com>
2024-01-16 14:35:19 +0300

    Adjust __init__ to allow change over between DBstorage and Filestorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an object of class FileStorage or DBStorage"""
 from os import getenv
-
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
-    from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
     storage.reload()
+
+# Importing model classes after storage instantiation
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+# Dictionary of model classes
+classes = {"User": User, "BaseModel": BaseModel,
+           "Place": Place, "State": State,
+           "City": City, "Amenity": Amenity,
+           "Review": Review}
