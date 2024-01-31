a89f573caf0a24d49b0e75d1b0dbdfbc09d4da58
Zablon Samba <zablonsamba@gmail.com>
2024-01-31 20:47:10 +0300

    Write a Bash script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -26,9 +26,9 @@ samba="
 </html>"
 echo "$samba" | sudo tee /data/web_static/releases/test/index.html
 # Remove symbolic link if exists (-f) before creating a new one
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Configure Nginx to serve alias content
-sed -i "\#server_name _;#a \
+sudo sed -i "\#server_name _;#a \
         location /hbnb_static {\
             alias /data/web_static/current/;\
         }" /etc/nginx/sites-enabled/default
