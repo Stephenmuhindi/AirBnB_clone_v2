72e7544cf5c310e2af2a9b9f03407e1b17022dd1
Zablon Samba <zablonsamba@gmail.com>
2024-02-02 02:08:55 +0300

    Puppet set up for deployment

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,15 +1,19 @@
 # Puppet Manifest that configures a server in 
 # readiness for deployment
 package { 'nginx':
-  ensure   => 'present',
-  provider => 'apt',
-} 
+  ensure  => 'present',
+  require => Exec['update_apt_store'],
+}
+
+exec { 'update_apt_store':
+  command => '/usr/bin/apt-get update',
+}
+
 
 file { '/data':
   ensure  => 'directory',
   require => Package['nginx']
 } 
-
 file { '/data/web_static':
   ensure  => 'directory',
   require => File['/data']
@@ -27,23 +31,12 @@ file { '/data/web_static':
   target => '/data/web_static/releases/test',
 } -> exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/',
-} -> file { '/var/www':
-  ensure => 'directory',
-} -> file { '/var/www/html':
-  ensure => 'directory',
-} -> file { '/var/www/html/index.html':
-  ensure  => 'present',
-  content => "Holberton School Nginx\n",
-} -> file { '/var/www/html/404.html':
-  ensure  => 'present',
-  content => "Ceci n'est pas une page\n",
 } -> file_line { 'add_hbnb_static_location':
   path    => '/etc/nginx/sites-enabled/default',
   line    => '        location /hbnb_static {',
   match   => '#server_name _;',
   after   => '#a',
   content => '            alias /data/web_static/current/;',
-} -> exec { 'nginx restart':
-  command => 'sudo /etc/init.d/nginx restart',
-  path    => '/etc/init.d/',
+} -> exec { 'restart_nginx':
+  command => '/usr/sbin/service nginx restart',
 }
