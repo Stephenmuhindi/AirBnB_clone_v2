6364da3b7a63e45656b801ffc4d85e7b6b7a2936
Zablon Samba <zablonsamba@gmail.com>
2024-02-01 19:11:09 +0300

    Write a Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -27,25 +27,23 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to web servers."""
+    """Distribute archive"""
     if exists(archive_path):
-        archive_filename = archive_path.split("/")[1]
-        without_ext = archive_filename.split(".")[0]
-        tmp_path = "/tmp/" + archive_filename
-        new_version = "/data/web_static/releases/" + without_ext
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
 
-        # Upload the archive to the /tmp/ directory of the web server
-        put(archive_filename, tmp_path)
-        sudo("mkdir -p {}".format(new_version))
-        sudo("tar -xzf {} -C {}".format(archive_filename, new_version))
-        sudo("rm {}".format(tmp_path))
-        sudo("mv {}/web_static/* {}".format(new_version,
-                                            new_version))
-        sudo("rm -rf {}/web_static".format(new_version))
-        sudo("rm -rf /data/web_static/current")
-        # Create a new symbolic link
-        sudo("ln -s {} /data/web_static/current".format(new_version))
         print("New version deployed!")
         return True
-    else:
-        return False
+
+    return False
