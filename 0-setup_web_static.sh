5747b95f37cdd3ddee2953f0023569cb19267c0f
Zablon Samba <zablonsamba@gmail.com>
2024-02-02 21:51:07 +0300

    Set Up Nginx for deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,19 +2,13 @@
 # Bash script that sets up your web servers for the deployment of web_static
 sudo apt-get -y update && sudo apt-get -y upgrade
 # Check if Nginx is installed, if not proceed to install
-if ! command -v nginx &> /dev/null; then
-    sudo apt-get -y install nginx
-fi
+sudo apt-get -y install nginx
 sudo ufw allow 'nginx HTTP'
 sudo service nginx start
 # Create directories if they dont exist, -p ensures
 # parent dirs are created as well
-if [ ! -d "/data/web_static/releases/test/" ]; then
-    sudo mkdir -p /data/web_static/releases/test/
-fi
-if [ ! -d "/data/web_static/shared/" ]; then
-    sudo mkdir -p /data/web_static/shared/
-fi
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 # Fake HTML file created
 samba="
 <html>
