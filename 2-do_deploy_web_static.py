53177aaf54320fe09c0730314fa57a020e78e627
Zablon Samba <zablonsamba@gmail.com>
2024-02-03 08:58:46 +0300

    Check why verbose aint working

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,25 +18,18 @@ def do_deploy(archive_path):
         put(archive_path, '/tmp/')
 
         timestamp = archive_path[-1].split('.')[0]
-        run('sudo mkdir -p /data/web_static/releases/web_static_{}/'
-            .format(timestamp))
+        run('sudo mkdir -p /data/web_static/releases/web_static_{}/'.format(timestamp))
 
         # Uncompress archive, delete archive, Move files into Host
         # web_static then remove the src web_static dir
-        run('sudo tar -vxzf /tmp/web_static_{}.tgz -C \
-            /data/web_static/releases/web_static_{}/'
-            .format(timestamp, timestamp))
+        run('sudo tar -vxzf /tmp/web_static_{}.tgz -C /data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
         run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
-        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
-            /data/web_static/releases/web_static_{}/'
-            .format(timestamp, timestamp))
-        run('sudo rm -rf /data/web_static/releases/web_static_{}/web_static'
-            .format(timestamp))
+        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* /data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+        run('sudo rm -rf /data/web_static/releases/web_static_{}/web_static'.format(timestamp))
 
         # Delete pre-existing sym link and re-establish
         run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s /data/web_static/releases/web_static_{}/ \
-            /data/web_static/current'.format(timestamp))
+        run('sudo ln -s /data/web_static/releases/web_static_{}/ /data/web_static/current'.format(timestamp))
     except Exception as e:
         print(f"An error occurred: {e}")
         return False
