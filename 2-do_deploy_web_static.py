61a94de8b752e5603241a7581755c86ca6e24c0a
Zablon Samba <zablonsamba@gmail.com>
2024-02-03 09:23:29 +0300

    Debug again

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,30 +10,24 @@ env.user = "ubuntu"
 
 def do_deploy(archive_path):
     """Deploy web files to server"""
-    try:
-        if not exists(archive_path):
-            return False
-
-        # Upload archive
-        put(archive_path, '/tmp/')
-
-        timestamp = archive_path.split('.')[0][-14:]
-        print(timestamp)
-        sudo('mkdir -p /data/web_static/releases/web_static_{}/'.format(timestamp))
-
-        # Uncompress archive, delete archive, Move files into Host
-        # web_static then remove the src web_static dir
-        sudo('tar -vxzf /tmp/web_static_{}.tgz -C /data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
-        sudo('rm /tmp/web_static_{}.tgz'.format(timestamp))
-        sudo('mv /data/web_static/releases/web_static_{}/web_static/* /data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
-        sudo('rm -rf /data/web_static/releases/web_static_{}/web_static'.format(timestamp))
-
-        # Delete pre-existing sym link and re-establish
-        sudo('rm -rf /data/web_static/current')
-        sudo('ln -s /data/web_static/releases/web_static_{}/ /data/web_static/current'.format(timestamp))
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    if not exists(archive_path):
         return False
 
-    # Returns True if successful
-    return True
+    # Upload archive
+    put(archive_path, '/tmp/')
+
+    timestamp = archive_path.split('.')[0][-14:]
+    print(timestamp)
+    sudo('mkdir -p /data/web_static/releases/web_static_{}/'.format(timestamp))
+
+    # Uncompress archive, delete archive, Move files into Host
+    # web_static then remove the src web_static dir
+    sudo('tar -vxzf /tmp/web_static_{}.tgz -C /data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+    sudo('rm /tmp/web_static_{}.tgz'.format(timestamp))
+    sudo('mv /data/web_static/releases/web_static_{}/web_static/* /data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+    sudo('rm -rf /data/web_static/releases/web_static_{}/web_static'.format(timestamp))
+
+    # Delete pre-existing sym link and re-establish
+    sudo('rm -rf /data/web_static/current')
+    sudo('ln -s /data/web_static/releases/web_static_{}/ /data/web_static/current'.format(timestamp))
+
