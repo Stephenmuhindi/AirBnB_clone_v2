1fa1e6bd395c619572887b3664a7ca3db7d5ee78
Stephenmuhindi <bteneson42@gmail.com>
2024-02-04 11:19:35 -0800

    alaa

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,14 +4,14 @@ rada ya hii namba 2?
 """
 from os.path import exists, basename, splitext
 from datetime import datetime
-from fabric.api import env, task, put, local, run
+from fabric.api import env, task, put, local, sudo, run
 env.use_ssh_config = True
-env.hosts = ["54.237.61.71", "54.146.64.127"]
+env.hosts = ["54.208.23.155", "34.239.255.201"]
 
 
 def do_pack():
     """
-    Function Docs
+    Fun
     """
     file = "versions/web_static_{}.tgz".format(
             datetime.now().strftime('%Y%m%d%H%M%S')
@@ -24,27 +24,27 @@ def do_pack():
 
 def do_deploy(archive_path):
     """
-    Function Docs
+    fungshon documendation
     """
     try:
         if not exists(archive_path):
             return False
-        ext = basename(archive_path)
-        no_ext, ext = splitext(ext)
+        exte = basename(archive_path)
+        no_ext, ext = splitext(exte)
         web_static_dir = "/data/web_static/releases/"
         put(archive_path, "/tmp/")
         commands = [
                 "rm -rf {}{}/".format(web_static_dir, no_ext),
                 "mkdir -p {}{}/".format(web_static_dir, no_ext),
-                "tar -xzf /tmp/{} -C {}{}/".format(ext, web_static_dir, no_ext),
-                "rm /tmp/{}".format(ext),
+                "tar -xzf /tmp/{} -C {}{}/".format(exte, web_static_dir, no_ext),
+                "rm /tmp/{}".format(archive_path),
                 "mv {0}{1}/web_static/* {0}{1}/".format(web_static_dir, no_ext),
                 "rm -rf {}{}/web_static".format(web_static_dir, no_ext),
                 "rm -rf /data/web_static/current",
                 "ln -s {}{}/ /data/web_static/current".format(web_static_dir, no_ext),
                 ]
         for command in commands:
-            run(command)
+            sudo(command)
         print("New version deployed!")
         return True
     except Exception:
