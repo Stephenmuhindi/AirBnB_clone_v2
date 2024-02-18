c53b125f6d71acaecf4a4fb2e2d8bd1e7f35c8d0
Stephenmuhindi <bteneson42@gmail.com>
2024-02-18 09:14:15 -0800

    7-dump.sql

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,23 +4,22 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+
+
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """html thingy"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def dispose(exception):
-    """ close sess"""
+def teardown(self):
+    """close sess"""
     storage.close()
 
 
-@app.route('/states_list')
-def states():
-    """pr"int list """
-    states = storage.all(State)
-    states_list = list(states.values())
-    return render_template('7-states_list.html', states=states_list)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
