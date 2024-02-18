1531b2565a8f131ca72de39e9ff36ab54b222c61
Stephenmuhindi <bteneson42@gmail.com>
2024-02-17 21:50:39 -0800

    7-dump.sql

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
 """
-flask web app`
+Flask web app
 """
-from models import storage
-from flask import Flask
-from flask import render_template
 
+from flask import Flask, render_template
+from models import *
+from models import storage
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """
-    state by order
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    """display in order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
+def teardown_db(exception):
     """close sess"""
     storage.close()
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
